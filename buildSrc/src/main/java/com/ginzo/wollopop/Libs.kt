4d7ccb6f22c9b439710f2e6b1858fa02228cf782
Gerard <gserrano@gyngi.com>
2019-10-03 15:53:40 +0200

    Configuring gradle

--- buildSrc/src/main/java/com/ginzo/wollopop/Libs.kt
+++ buildSrc/src/main/java/com/ginzo/wollopop/Libs.kt
@@ -1,3 +1,29 @@
 package com.ginzo.wollopop
 
-object Libs {}
+object Libs {
+    object kotlin {
+        val kotlinVersion = "1.3.50"
+
+        val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion"
+
+        object gradle {
+            val core = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+        }
+    }
+
+    val coreKtx = "androidx.core:core-ktx:1.0.2"
+
+    object androidx {
+        val appCompat = "androidx.appcompat:appcompat:1.0.2"
+    }
+
+    object google {
+        val material = "com.google.android.material:material:1.0.0"
+    }
+}
+
+object Conf {
+    const val compileSdkVersion = 28
+    const val targetSdkVersion = 28
+    const val minSdkVersion = 21
+}
