779a6c796b9b40d4a98a062ae60001482560dbd1
Gerard <gserrano@gyngi.com>
2019-10-06 23:24:32 +0200

    Create component manager to manage the dagger components

---
+++ app/src/main/java/com/ginzo/app/WollopopApplication.kt
@@ -0,0 +1,14 @@
+package com.ginzo.app
+
+import android.app.Application
+import com.ginzo.app.di.ComponentManager
+
+class WollopopApplication : Application() {
+
+    private lateinit var componentManager: ComponentManager
+
+    override fun onCreate() {
+        super.onCreate()
+        componentManager = ComponentManager(this)
+    }
+}
---
+++ app/src/main/java/com/ginzo/app/di/ComponentManager.kt
@@ -0,0 +1,12 @@
+package com.ginzo.app.di
+
+import android.app.Application
+import com.ginzo.remote.DaggerRemoteComponent
+import com.ginzo.remote.RemoteComponent
+
+class ComponentManager(application: Application) {
+
+    internal val remoteComponent: RemoteComponent by lazy {
+        DaggerRemoteComponent.create()
+    }
+}
