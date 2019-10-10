fef492b99ffc0c8e2d097cdf02da154a0abfc297
Gerard <gserrano@gyngi.com>
2019-10-10 11:39:48 +0200

    Create product detail activity

---
+++ features/productdetails/productdetails-presentation/src/main/java/com/ginzo/features/productdetails/productdetails_presentation/ProductDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.ginzo.features.productdetails.productdetails_presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDetailActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_product_detail)
+  }
+}
